e9022ed6baaa69c965fab897c6b5af8d2353b2d5
A.L <xenide@users.noreply.github.com>
2024-04-14 10:20:20 +0200

    refactor: change func sig for `updatePrice`

--- src/ReservoirPriceCache.sol
+++ src/ReservoirPriceCache.sol
@@ -35,7 +35,7 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
     event TwapPeriod(uint256 newPeriod);
     event PriceDeviationThreshold(uint256 newThreshold);
     event RewardMultiplier(uint256 newMultiplier);
-    event Price(address indexed pair, uint256 price);
+    event Price(address token0, address token1, uint256 price);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //                                       ERRORS                                              //
@@ -43,6 +43,7 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
 
     error RPC_THRESHOLD_TOO_HIGH();
     error RPC_TWAP_PERIOD_TOO_HIGH();
+    error RPC_SAME_TOKEN();
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //                                        STORAGE                                            //
@@ -58,12 +59,12 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
     /// beyond the `priceDeviationThreshold`
     uint64 public rewardMultiplier;
 
-    /// @notice TWAP period for querying the oracle
+    /// @notice TWAP period for querying the oracle in seconds
     uint64 public twapPeriod;
 
-    // for a certain pair, regardless of the curve id, the latest cached price of token1/token0
+    // the latest cached TWAP of token1/token0, where address of token0 is strictly less than address of token1
     // calculate reciprocal to for price of token0/token1
-    mapping(address => uint256) public priceCache;
+    mapping(address token0 => mapping(address token1 => uint256 price)) public priceCache;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //                                CONSTRUCTOR, FALLBACKS                                     //
@@ -79,6 +80,15 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
     /// @dev contract will hold native tokens to be distributed as gas bounty for updating the prices
     /// anyone can contribute native tokens to this contract
     receive() external payable { }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //                                       MODIFIERS                                           //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    modifier validateTokens(address aTokenA, address aTokenB) {
+        if (aTokenA == aTokenB) revert RPC_SAME_TOKEN();
+        _;
+    }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //                                   PUBLIC FUNCTIONS                                        //
@@ -119,24 +129,25 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
         return "RESERVOIR PRICE CACHE";
     }
 
-    function getQuote(uint256 aAmount, address aBase, address aQuote) external view returns (uint256 rOut) {
-        // figure out which pair to use for the given base quote asset
-        // where should the lookup table reside? in this contract or in the main oracle contract?
-        (address lToken0, address lToken1) = aBase.sortTokens(aQuote);
-
-        address lPair = lookup[aBase][aQuote];
-
-        uint256 lPrice = priceCache[lPair];
-
-        //
+    function getQuote(uint256 aAmount, address aBase, address aQuote)
+        external
+        view
+        validateTokens(aBase, aQuote)
+        nonReentrant
+        returns (uint256 rOut)
+    {
+        rOut = _getQuote(aAmount, aBase, aQuote);
     }
 
     function getQuotes(uint256 aAmount, address aBase, address aQuote)
         external
         view
+        validateTokens(aBase, aQuote)
+        nonReentrant
         returns (uint256 rBidOut, uint256 rAskOut)
     {
-        return 0;
+        uint256 lResult = _getQuote(aAmount, aBase, aQuote);
+        (rBidOut, rAskOut) = (lResult, lResult);
     }
 
     // price update related functions
@@ -149,32 +160,32 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
         return address(this).balance;
     }
 
-    /// @dev we do not allow specifying which address gets the reward, to save on calldata gas
-    function updatePrice(address aPair) external nonReentrant {
-        ReservoirPair lPair = ReservoirPair(aPair);
-
-        // validate that the pair is indeed ours and whitelisted
+    // @param aRewardRecipient The beneficiary of the reward. Must implement the receive function if is a contract address
+    function updatePrice(address aTokenA, address aTokenB, address aRewardRecipient)
+        external
+        validateTokens(aTokenA, aTokenB)
+        nonReentrant
+    {
+        (address lToken0, address lToken1) = aTokenA.sortTokens(aTokenB);
 
         OracleAverageQuery[] memory lQueries;
         lQueries[0] = OracleAverageQuery(
             Variable.RAW_PRICE,
-            address(lPair.token0()),
-            address(lPair.token1()),
+            lToken0,
+            lToken1,
             twapPeriod,
             0 // now
         );
 
-        // reads new price from pair
         uint256 lNewPrice = oracle.getTimeWeightedAverage(lQueries)[0];
 
-        // determine if price has moved beyond the threshold
-        // reward caller if so
-        if (_calcPercentageDiff(lNewPrice, priceCache[aPair]) >= priceDeviationThreshold) {
-            _rewardUpdater(msg.sender);
+        // determine if price has moved beyond the threshold, and pay out reward if so
+        if (_calcPercentageDiff(priceCache[lToken0][lToken1], lNewPrice) >= priceDeviationThreshold) {
+            _rewardUpdater(aRewardRecipient);
         }
 
-        priceCache[aPair] = lNewPrice;
-        emit Price(aPair, lNewPrice);
+        priceCache[lToken0][lToken1] = lNewPrice;
+        emit Price(lToken0, lToken1, lNewPrice);
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -183,6 +194,8 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
 
     // TODO: replace this with safe, audited lib function
     function _calcPercentageDiff(uint256 aOriginal, uint256 aNew) internal pure returns (uint256) {
+        if (aOriginal == 0) return 0;
+
         if (aOriginal > aNew) {
             return (aOriginal - aNew) * 1e18 / aOriginal;
         } else {
@@ -200,5 +213,20 @@ contract ReservoirPriceCache is Owned(msg.sender), ReentrancyGuard, IPriceOracle
         if (lPayoutAmt <= address(this).balance) {
             payable(lRecipient).transfer(lPayoutAmt);
         } else { } // do nothing if lPayoutAmt is greater than the balance
+    }
+
+    function _getQuote(uint256 aAmount, address aBase, address aQuote) internal view returns (uint256 rOut) {
+        (address lToken0, address lToken1) = aBase.sortTokens(aQuote);
+
+        uint256 lPrice = priceCache[lToken0][lToken1];
+
+        if (lPrice == 0) revert PO_NoPath();
+
+        // can build a cache of decimals to save on making these external calls?
+        uint256 lBaseDecimals = IERC20(aBase).decimals();
+        uint256 lQuoteDecimals = IERC20(aQuote).decimals();
+
+        // quoteAmountOut = baseAmountIn * wadPrice * quoteDecimalScale / baseDecimalScale / WAD
+        rOut = (aAmount * lPrice).fullMulDiv(10 ** lQuoteDecimals, (10 ** lBaseDecimals) * WAD);
     }
 }
