c6f4277a9f5b2bf044bd9393be8edf060ade767e
A.L. <xenide@users.noreply.github.com>
2024-07-08 20:55:31 +0200

    lint: add solhint disable

--- test/mock/GasBuster.sol
+++ test/mock/GasBuster.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 contract GasBuster {
     // Allow the contract to receive ETH
     receive() external payable {
-        while (true) {
+        while (true) { // solhint-disable-line no-empty-blocks
             // This loop will continue until all gas is consumed
         }
     }
